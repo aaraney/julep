b27b4cbae5fc9c3b8232d108a97ab3c52255b78b
Austin Raney <aaraney@protonmail.com>
2022-09-06 17:14:44 -0400

    add image map

---
+++ image_map/image_map.go
@@ -0,0 +1,24 @@
+package image_map
+
+import (
+	"github.com/aaraney/inlet/image"
+)
+
+// assumes that all image _names_ are unique.
+// image name does not include registry name.
+// so, `google/cadvisor`'s image name is `cadvisor`.
+type ImageMap map[string][]image.Image
+
+func (m ImageMap) Insert(key string, images ...image.Image) {
+	if !m.Exists(key) {
+		m[key] = images
+		return
+	}
+
+	m[key] = append(m[key], images...)
+}
+
+func (m ImageMap) Exists(key string) bool {
+	_, ok := m[key]
+	return ok
+}
---
+++ image_map/image_map_test.go
@@ -0,0 +1,37 @@
+package image_map
+
+import (
+	"testing"
+
+	"github.com/aaraney/inlet/image"
+)
+
+func TestInsert(t *testing.T) {
+	i := make(ImageMap)
+	key := "source"
+	value := image.Image{Name: "target"}
+
+	i.Insert(key, value)
+	if i[key][0] != value {
+		t.Logf("map[%q][0] should equal %#v", key, value)
+	}
+
+	i.Insert(key, value)
+	if i[key][1] != value {
+		t.Logf("map[%q][1] should equal %#v", key, value)
+	}
+
+	if len(i[key]) != 2 {
+		t.Logf("len(map[%q]) != 2. map[%q] = %#v", key, key, value)
+	}
+}
+
+func TestExists(t *testing.T) {
+	i := make(ImageMap)
+	key := "source"
+	i[key] = []image.Image{{Name: "target"}}
+
+	if !i.Exists(key) {
+		t.Logf("key, %q, should exist in image map, %#v", key, i)
+	}
+}
