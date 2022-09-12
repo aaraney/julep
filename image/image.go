66ee7fafefe37ad0e107c0c79a756f18ab07293b
Austin Raney <aaraney@protonmail.com>
2022-09-11 21:46:52 -0400

    add ImagePair struct. still trying to decide on the api that im going for

--- image/image.go
+++ image/image.go
@@ -1,5 +1,7 @@
 package image
 
+import "io"
+
 const Prefix = "Dockerfile."
 
 type Image struct {
@@ -7,4 +9,19 @@ type Image struct {
 	Tag      string
 	Hash     string
 	Registry string
+}
+
+type ImagePair struct {
+	SourceImage Image
+	Name        string
+	Path        string
+}
+
+func NewImagePair(image_path string, image_reader io.Reader) ImagePair {
+	source, _ := ImageSource(image_reader)
+	return ImagePair{
+		Name:        BuildName(image_path),
+		Path:        image_path,
+		SourceImage: source,
+	}
 }
