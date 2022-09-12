180c92dfc1fcd950e3596d9bbbabaaedba87c5c2
Austin Raney <aaraney@protonmail.com>
2022-09-12 17:26:47 -0400

    add non-thread safe queue

---
+++ queue/queue.go
@@ -0,0 +1,26 @@
+package queue
+
+import "errors"
+
+// NOT THREAD SAFE
+type Queue[T any] struct {
+	queue []T
+}
+
+func (q *Queue[T]) Push(items ...T) {
+	q.queue = append(q.queue, items...)
+}
+
+func (q *Queue[T]) Pop() (T, error) {
+	if q.Empty() {
+		return *new(T), errors.New("queue empty")
+	}
+
+	item := q.queue[0]
+	q.queue = q.queue[1:]
+	return item, nil
+}
+
+func (q *Queue[T]) Empty() bool {
+	return len(q.queue) == 0
+}
