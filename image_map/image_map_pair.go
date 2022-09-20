65042db5055ddbac298177ca325a40fd473475ae
Austin Raney <aaraney@protonmail.com>
2022-09-20 15:31:12 -0400

    add NewImageMapPair function

--- image_map/image_map_pair.go
+++ image_map/image_map_pair.go
@@ -1,6 +1,12 @@
 package image_map
 
 import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
 	"github.com/aaraney/inlet/image"
 )
 
@@ -28,4 +34,47 @@ func (m ImageMapPair) Insert(key string, images ...image.ImagePair) {
 func (m ImageMapPair) Exists(key string) bool {
 	_, ok := m[key]
 	return ok
+}
+
+func NewImageMapPair(rootDir string) ImageMapPair {
+	dockerfiles := make(chan string)
+
+	imgs := make(ImageMapPair)
+	wg := sync.WaitGroup{}
+
+	N_WORKERS := 4
+
+	for i := 0; i < N_WORKERS; i++ {
+		wg.Add(1)
+		go func() {
+			for dockerfile := range dockerfiles {
+
+				f, err := os.Open(dockerfile)
+				if err != nil {
+					continue
+				}
+
+				pair := image.NewImagePair(dockerfile, f)
+
+				f.Close()
+				imgs.InsertOne(pair)
+			}
+			wg.Done()
+		}()
+
+	}
+
+	findDockerfiles(rootDir, dockerfiles)
+	wg.Wait()
+	return imgs
+}
+
+func findDockerfiles(location string, dockerfiles chan<- string) {
+	filepath.WalkDir(location, func(path string, d fs.DirEntry, err error) error {
+		if strings.HasPrefix(d.Name(), "Dockerfile") {
+			dockerfiles <- path
+		}
+		return err
+	})
+	close(dockerfiles)
 }
