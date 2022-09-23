bf0e27088217d87671ef709e28c0beb90903eefb
Austin Raney <aaraney@protonmail.com>
2022-09-23 14:11:04 -0400

    add worker shim. runs jobs and is reactive to cancellation

---
+++ pkg/build/worker_shim.go
@@ -0,0 +1,63 @@
+package build
+
+type Doer[T any] interface {
+	Do(T)
+	Cancel(T)
+}
+
+func WorkerShim[T any](doer Doer[T], jobs <-chan T, cancel chan chan struct{}) {
+	done := make(chan struct{}, 1)
+
+	for {
+		select {
+
+		case job := <-jobs:
+
+			// start job in background
+			go func(job T) {
+				defer func() { done <- struct{}{} }()
+				doer.Do(job)
+			}(job)
+
+			// wait for the job to be done
+			// or
+			// cancel signal
+			select {
+			case <-done:
+
+			// job is in progress
+			case c := <-cancel:
+				// should block until job has been canceled
+				doer.Cancel(job)
+				// consider deadline function in future
+				// Deadline(func() { doer.Cancel(job) }, time.Duration(2)*time.Second)
+				c <- struct{}{}
+				return
+			}
+
+		case c := <-cancel:
+			c <- struct{}{}
+			return
+		}
+	}
+}
+
+// Deadline calls fn in go routine, chan is filled with true if fn finishes before timeout. Chan is
+// filled with false if timeout expires before fn returns.
+// func Deadline(fn func(), timeout time.Duration) <-chan bool {
+// 	done := make(chan struct{})
+// 	finished := make(chan bool, 1)
+// 	go func() {
+// 		defer func() { done <- struct{}{} }()
+// 		fn()
+// 	}()
+
+// 	select {
+// 	case <-time.After(timeout):
+// 		finished <- false
+// 	case <-done:
+// 		finished <- true
+// 	}
+
+// 	return finished
+// }
---
+++ pkg/build/worker_shim_test.go
@@ -0,0 +1,85 @@
+package build
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+type MockDoer struct {
+	stop chan struct{}
+	done chan struct{}
+}
+
+func NewMockDoer() MockDoer {
+	return MockDoer{stop: make(chan struct{}), done: make(chan struct{}, 1)}
+}
+
+func (m MockDoer) reset() {
+	select {
+	case <-m.done:
+	default:
+	}
+}
+
+func (m MockDoer) Do(sleep_time int) {
+	m.reset()
+
+	select {
+	case <-time.After(time.Duration(sleep_time) * time.Millisecond):
+		// fmt.Printf("slept %d ms\n", sleep_time)
+		m.done <- struct{}{}
+	case <-m.stop:
+		fmt.Println("stopped")
+	}
+}
+
+func (m MockDoer) Cancel(sleep_time int) {
+	select {
+	case <-m.done:
+		fmt.Println("already done")
+	case m.stop <- struct{}{}:
+	// max time to wait to try and stop Doer
+	case <-time.After(time.Duration(2) * time.Second):
+		fmt.Println("timed out")
+	}
+}
+
+func TestWorkerShim(t *testing.T) {
+	cancel := make(chan chan struct{})
+	jobs := make(chan int)
+
+	workers := 5
+
+	for i := 0; i < workers; i++ {
+		doer := NewMockDoer()
+		go WorkerShim[int](doer, jobs, cancel)
+	}
+
+	go func() {
+		for {
+			jobs <- rand.Intn(150) + 50
+		}
+	}()
+
+	// simulate wait time
+	time.Sleep(time.Duration(rand.Intn(300)+200) * time.Millisecond)
+
+	// cancel the workers
+	canceled := make(chan struct{})
+	for i := 0; i < workers; i++ {
+		c := make(chan struct{})
+		cancel <- c
+		go func(c chan struct{}) { canceled <- <-c }(c)
+	}
+
+	for i := 0; i < workers; i++ {
+		<-canceled
+	}
+	select {
+	case <-canceled:
+		t.Fatal("canceled chan should be empty")
+	default:
+	}
+}
